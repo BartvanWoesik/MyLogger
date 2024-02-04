9ec309f4d34295aa0aa50a714a714b8c7ba5e864
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-04 15:38:19 +0100

    update base logger

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_project_name",
-    version="0.1.0",
+    name="MyLogger",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         # List your project dependencies here
