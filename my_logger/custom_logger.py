e1a156d06c8eb763762e15e3037b5b5e3d59ae38
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-19 12:47:34 +0100

    remove test from custom_logger

--- my_logger/custom_logger.py
+++ my_logger/custom_logger.py
@@ -49,21 +49,3 @@ logging.setLoggerClass(MyLogger)
 logger = logging.getLogger(__name__)
 logger = logger.setup_logging()
 logger.info("Logger is set up.")
-
-
-def main():
-    available_config()
-
-    logger.debug("debug message", extra={"x": "hello"})
-    logger.info("info message")
-    logger.warning("warning message")
-    logger.error("error message")
-    logger.critical("critical message")
-    try:
-        1 / 0
-    except ZeroDivisionError:
-        logger.exception("exception message")
-
-
-if __name__ == "__main__":
-    main()
